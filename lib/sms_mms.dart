83433d1906548370fce0a323d0dbd47bdf06153a
johannesvuorinen <johannes@frogmindgames.com>
2024-08-15 08:44:39 +0300

    Update sms_mms.dart

--- lib/sms_mms.dart
+++ lib/sms_mms.dart
@@ -5,12 +5,14 @@ class SmsMms {
   static Future<void> send({
     required List<String> recipients,
     String? filePath,
+    String type,
     required String message,
   }) async {
     return SmsMmsPlatform.instance.sendMms(
       phones: recipients,
       text: message,
       filePath: filePath,
+      type: type
     );
   }
 }
